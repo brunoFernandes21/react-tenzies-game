3a826265fe203e2280e19ee45e87b3fb7e4dcd2b
brunoFernandes21 <47353649+brunoFernandes21@users.noreply.github.com>
2023-01-24 22:06:53 +0000

    Added new features
    1 - Add green background to die that is held,
    2 - dice that are held will not roll when roll btn is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,59 @@
 import { useState } from 'react'
 import Die from './Die'
+import { nanoid } from 'nanoid'
 
 const App = () => {
+
+  const generateNewDie = () => {
+    return {
+      value: Math.ceil(Math.random() * 6), 
+      isHeld: false, 
+      id: nanoid()
+    }
+  }
 
   const allNewDice = () => {
     //create new array to hold my numbers
     const newDice = []
     //loop through 10 times
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i <10; i++){
       //push a random number from 1-6 to my array
-      newDice.push(Math.ceil(Math.random() * 6))
+      newDice.push(generateNewDie())
     }
-    //return 
     return newDice
   }
+
   const [ dice, setDice ] = useState(allNewDice())
 
   const rollDice = () => {
-    setDice(allNewDice())
+    // setDice(allNewDice())
+    setDice(oldDice => oldDice.map((die) => {
+      return die.isHeld ? die : generateNewDie()
+    }))
   }
   
-  const diceElements = dice.map((die) => <Die value={die}/>)
+  const holdDice = (id) => {
+    setDice((oldDice) => oldDice.map((die) => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : 
+      die
+    }))
+  }
+  const diceElements = dice.map((die) => (
+  <Die 
+  key={die.id} 
+  value={die.value} 
+  isHeld={die.isHeld} 
+  holdDice={()=> holdDice(die.id)}
+  />
+  ))
 
   return (
     <main>
+      <h1 className='title'>Tenzies</h1>
+      <p className='instructions'>Roll until all dice are the same. 
+        Click each die to freeze it at its current value
+        between rolls .
+        </p>
       <div className='dice__container'>
         {diceElements}
       </div>
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const Die = (props) => {
+  const { value, isHeld, holdDice } = props
+
+  const styles = {
+    backgroundColor: isHeld ? '#59E391' : 'wheat'
+  }
   return (
-    <div className='die-face'>
-        <h2 className='die__numb'>{props.value}</h2>
+    <div 
+		className='die-face' 
+		style={styles} 
+		onClick={holdDice}
+		>
+      <h2 className='die__numb'>{value}</h2> 
     </div>
   )
-}
+} 
 
 export default Die
